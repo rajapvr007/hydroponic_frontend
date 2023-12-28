a9f2b4f04e6529181388e0a8b31733f34c53c7a5
K RAJA <cse.raja007@gmail.com>
2023-12-28 22:03:24 +0530

    first push

---
+++ hydroponic/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar/Navbar"
+
+const App = () => {
+  return(
+    <>
+    <Navbar/> 
+    <div className="bg-green-500 w-full h-screen"></div>
+    </>
+
+  )
+}
+
+export default App
